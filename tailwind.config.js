77acf3b8ff6949ad6b487d196025d7eb4f063a24
ysh <sunghe1016@naver.com>
2025-09-01 14:27:18 +0900

    feat: :sparkles: 초기 설정

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}", // src 폴더 내 모든 파일 체크
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+// vite.config.ts 파일
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from "path";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+  resolve: {
+    alias: {
+      api: path.resolve(__dirname, "src/api"),
+      types: path.resolve(__dirname, "src/types"),
+      config: path.resolve(__dirname, "src/config"),
+    },
+  },
+})
