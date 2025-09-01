bd1bdbd24f6d5f5c512872562c460fcc106c851f
ysh <sunghe1016@naver.com>
2025-09-01 18:35:37 +0900

    fix: 테일윈드 자동정렬 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}", // src 폴더 내 모든 파일 체크
-    ],
-    theme: {
-      extend: {},
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}', // src 폴더 내 모든 파일 체크
+  ],
+  theme: {
+    extend: {
+      boxShadow: {
+        problem: '0px 2px 6px 0px #00000014',
+      },
     },
-    plugins: [],
-  };
+  },
+  plugins: [],
+};
