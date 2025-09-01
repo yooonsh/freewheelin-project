399a6488f000764af67faa2437f028503a04cb40
ysh <sunghe1016@naver.com>
2025-09-01 18:36:27 +0900

    fix: component path 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,18 @@
 // vite.config.ts 파일
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      api: path.resolve(__dirname, "src/api"),
-      types: path.resolve(__dirname, "src/types"),
-      config: path.resolve(__dirname, "src/config"),
+      api: path.resolve(__dirname, 'src/api'),
+      types: path.resolve(__dirname, 'src/types'),
+      config: path.resolve(__dirname, 'src/config'),
+      components: path.resolve(__dirname, 'src/components'),
     },
   },
-})
+});
